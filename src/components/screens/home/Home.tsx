d5c6fcd65cb4368ba5aa259572a90926b973c662
Imron Mirzarasulov <imronimronov32@gmail.com>
2023-03-06 23:48:51 +0500

    add img and icons

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,10 +1,19 @@
-import Button from "@/common/Button/Button";
+import Image from 'next/image'
+
+import Button from '@/common/Button/Button'
+
 import Layout from '@/layout/Layout'
 
 const Home = () => {
 	return (
 		<Layout title={'Главная'}>
 			<Button>subscribe</Button>
+			<Image
+				src={'/img/registration.jpg'}
+				alt={'registration'}
+				width={100}
+				height={100}
+			/>
 		</Layout>
 	)
 }
